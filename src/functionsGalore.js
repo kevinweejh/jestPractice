121b98e19ff4251e1e95ebdc45f72cbcade77c42
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-02-21 17:53:13 +0800

    feat: add reverseString function

--- src/functionsGalore.js
+++ src/functionsGalore.js
@@ -1,4 +1,4 @@
-export default capitalise = (input) => {
+export const capitalise = (input) => {
     if (typeof input !== 'string') {
         throw Error('Incorrect input type, please provide a string.');
         return;
@@ -9,6 +9,22 @@ export default capitalise = (input) => {
     const outputArray = inputArray.slice();
 
     outputArray.splice(0,1,capitalisedFirstChar);
+    const outputString = outputArray.join('');
+
+    return outputString;
+}
+
+export const reverseString = (input) => {
+    if (typeof input !== 'string') {
+        throw Error('Incorrect input type, please provide a string.');
+        return;
+    }
+    if (input.length === 0) {
+        throw Error('Please provide a non-empty string.');
+        return;
+    }
+    const inputArray = input.split('');
+    const outputArray = inputArray.reverse();
     const outputString = outputArray.join('');
 
     return outputString;
