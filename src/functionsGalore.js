85247f20b7f3e34efa0c65d339f80578c5dbc56f
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-02-21 19:25:26 +0800

    feat: add caesarCypher() function

--- src/functionsGalore.js
+++ src/functionsGalore.js
@@ -80,3 +80,40 @@ export const calculator = {
         return firstNum * secondNum;
     }
 }
+
+export const caesarCipher = (plaintext, shiftFactor) => {
+    if (typeof plaintext !== 'string') {
+        throw Error('Incorrect input type, please provide a string.');
+        return;
+    }
+    if (plaintext.length === 0) {
+        throw Error('Please provide a non-empty string.');
+        return;
+    }
+    
+    const cyphertext = plaintext
+                        .split('') // Array of plainChars
+                        .map((plainChar) => shiftChar(plainChar, shiftFactor)) // Array of cypherChars
+                        .join('');
+
+    return cyphertext;
+}
+
+const shiftChar = (plainChar, shiftFactor) => {
+    const plainCode = plainChar.charCodeAt();
+    let cypherChar, cypherCode;
+
+    // To select the appropriate shift for lowercase vs uppercase alphabets
+    const codeSet = (plainCode) => (plainCode < 97 ? 65 : 97);
+
+    // Alphabets
+    if ((plainCode <= 122 && plainCode >= 97) || (plainCode <= 90 && plainCode >= 65)) { 
+        cypherCode = ((plainCode + shiftFactor - codeSet(plainCode)) % 26) + codeSet(plainCode);
+        cypherChar = String.fromCharCode(cypherCode);
+    } else { // Punctuations
+        cypherCode = plainCode;
+        cypherChar = String.fromCharCode(cypherCode);
+    }
+
+    return cypherChar;
+}
